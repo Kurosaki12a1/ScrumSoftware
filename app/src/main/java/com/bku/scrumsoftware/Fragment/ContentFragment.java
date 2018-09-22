e2624ad4f8d8a4d410c994e22713e92f594e79e0
Kurosaki12a1 <tyblackzs@gmail.com>
2018-09-22 20:50:52 +0700

    Update popup Detail

--- app/src/main/java/com/bku/scrumsoftware/Fragment/ContentFragment.java
+++ app/src/main/java/com/bku/scrumsoftware/Fragment/ContentFragment.java
@@ -1,12 +1,21 @@
 package com.bku.scrumsoftware.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bku.scrumsoftware.Activity.DetailActivity;
 import com.bku.scrumsoftware.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Welcome on 9/22/2018.
@@ -14,10 +23,33 @@ import com.bku.scrumsoftware.R;
 
 public class ContentFragment extends Fragment {
 
+   /* @BindView(R.id.cardViewMeetings)
+    CardView mMeeting;*/
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_content, container, false);
+        View view=inflater.inflate(R.layout.fragment_content, container, false);
+        ButterKnife.bind(this,view);
+        return null;
 
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+       /* mMeeting = view.findViewById(R.id.cardViewMeetings);
+        mMeeting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), DetailActivity.class);
+                i.putExtra("type","Meeting");
+                startActivity(i);
+            }
+        });*/
+    }
+
+    /*@OnClick(R.id.cardViewMeetings)
+    public void viewMeetings(){
+
+    }*/
 }
